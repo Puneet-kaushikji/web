1f44f6413086ed0e3160ac3c295de43c95245d80
Puneet-kaushikji <puneetkaushik177@gmail.com>
2024-07-05 11:44:21 +0530

    todo-self

--- react/todo-list1/src/components/MainContent.js
+++ react/todo-list1/src/components/MainContent.js
@@ -3,55 +3,112 @@ import React, { useState } from 'react';
 const MainContent = () => {
   const [newTask, setNewTask] = useState('');
   const [tasks, setTasks] = useState([]);
+  
 
-  const addTask = () => {
-    if (newTask.trim() !== '') {
-      setTasks([...tasks, newTask]);
-      setNewTask(''); // Clear the input field after adding the task
+  function addValue(){
+    if(newTask.trim() !== '') {
+    setTasks([...tasks , newTask]);
+    setNewTask('');
     }
-  };
+  }
 
-  const deleteTask = (indexToDelete) => {
-    setTasks(tasks.filter((_, index) => index !== indexToDelete));
-  };
 
-  return (
-    <div >
-      <div className='m-5 text-2xl flex justify-center items-center h-full w-full'>
-        <input 
-          type="text" 
+  function deleteVal(index) {
+    setTasks(tasks.filter((val,i) =>  i!==index));
+  }
+
+
+    return(
+      <>  
+      <div>
+        <div className='m-5 text-2xl flex justify-center items-center h-full w-full'>
+         <input 
+         type='text'
           value={newTask}
-          onChange={(e) => setNewTask(e.target.value)}
-          placeholder="Enter your task"
+          placeholder='Enter the Task'
+          onChange={ (e) => setNewTask(e.target.value)}
           className="bg-gray-600 placeholder-slate-100 rounded-md p-2"
         /> 
         <br />
-
         <button 
-          onClick={addTask}  
+          onClick={addValue} 
           className='border-solid border-2 border-black rounded-xl bg-green-400 text-2xl p-1'
         >
           ADD 
         </button>
-      </div>
-   
-      <ul className="list-disc flex justify-center items-center flex-col">
-        {tasks.map((task, index) => (
-          <li key={index} className="text-lg text-gray-800 flex items-center">
-            <div className=' border-4  bg-gray-200 m-1  w-64 rounded-md'>
-              {task}
+        </div>
+
+        <ul className="list-disc flex justify-center items-center flex-col">
+           {tasks.map((val ,i) =>(
+            <li key={i} 
+            className="text-lg text-gray-800 flex items-center">
+              <div 
+              className=' border-4  bg-gray-200 m-1  w-64 rounded-md'>
+                {val}
               </div>
-            <button 
-              onClick={() => deleteTask(index)} 
-              className="ml-2 bg-red-500 text-white rounded-md p-1"
-            >
-              Delete
-            </button>
+              <button  
+              onClick={ () =>deleteVal(i)}
+              className="ml-2 bg-red-500 text-white rounded-md p-1">
+                Delete
+              </button>
           </li>
-        ))}
-      </ul>
-    </div>
-  );
+
+           ))
+           }
+        </ul>
+      </div>
+      </>
+    );
+
+
+  // const addTask = () => {
+  //   if (newTask.trim() !== '') {
+  //     setTasks([...tasks, newTask]);
+  //     setNewTask(''); // Clear the input field after adding the task
+  //   }
+  // };
+
+  // const deleteTask = (indexToDelete) => {
+  //   setTasks(tasks.filter((_, index) => index !== indexToDelete));
+  // };
+
+  // return (
+  //   <div >
+  //     <div className='m-5 text-2xl flex justify-center items-center h-full w-full'>
+  //       <input 
+  //         type="text" 
+  //         value={newTask}
+  //         onChange={(e) => setNewTask(e.target.value)}
+  //         placeholder="Enter your task"
+  //         className="bg-gray-600 placeholder-slate-100 rounded-md p-2"
+  //       /> 
+  //       <br />
+
+  //       <button 
+  //         onClick={addTask}  
+  //         className='border-solid border-2 border-black rounded-xl bg-green-400 text-2xl p-1'
+  //       >
+  //         ADD 
+  //       </button>
+  //     </div>
+   
+  //     <ul className="list-disc flex justify-center items-center flex-col">
+  //       {tasks.map((task, index) => (
+  //         <li key={index} className="text-lg text-gray-800 flex items-center">
+  //           <div className=' border-4  bg-gray-200 m-1  w-64 rounded-md'>
+  //             {task}
+  //             </div>
+  //           <button 
+  //             onClick={() => deleteTask(index)} 
+  //             className="ml-2 bg-red-500 text-white rounded-md p-1"
+  //           >
+  //             Delete
+  //           </button>
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   </div>
+  // );
 };
 
 export default MainContent;
